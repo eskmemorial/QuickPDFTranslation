1f19e6030d447c6ca6ea2aeeb872b87d83d30fab
eskmemorial <54440061+eskmemorial@users.noreply.github.com>
2020-03-12 08:46:08 +0900

    fix bug when open pdf in file://

--- background.js
+++ background.js
@@ -4,11 +4,19 @@ chrome.runtime.onMessage.addListener(
 
         if (message.type === "openPDFInViewer") {
 
+            if (message.value.url.startsWith("file:///")) {
+                message.value.url = message.value.url.substring("file:///".length, message.value.url.length);
+            }
+
             chrome.tabs.create({
                 windowId: sender.tab.windowId,
-                index: sender.tab.index,
+                //index: sender.tab.index,
                 openerTabId: sender.tab.id,
                 url: `extension://${chrome.runtime.id}/pdf.js/web/viewer.html?file=${message.value.url}`
+            });
+
+            chrome.tabs.remove({
+                tabIds: sender.tab.id,
             });
 
             return true;
